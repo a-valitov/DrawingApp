826169ab559c0c608b4244cb66c37cabab94f955
Artyom Valitov <valitov.artem.pro@yandex.ru>
2021-06-11 20:04:14 +0500

    BrushSizeChooserDialog realized

--- app/src/main/java/com/avalitov/drawingapp/DrawingView.kt
+++ app/src/main/java/com/avalitov/drawingapp/DrawingView.kt
@@ -3,6 +3,7 @@ package com.avalitov.drawingapp
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.MotionEvent
 import android.view.View
 
@@ -31,7 +32,7 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
         mDrawPaint!!.strokeJoin = Paint.Join.ROUND
         mDrawPaint!!.strokeCap = Paint.Cap.ROUND
         mCanvasPaint = Paint(Paint.DITHER_FLAG)
-        mBrushSize = 20.toFloat()
+//        mBrushSize = 20.toFloat()
 
     }
 
@@ -93,6 +94,14 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
 
         return true
     }
+
+    fun setBrushSize(newSize: Float){
+        //set brush size accordingly to display of the device
+        mBrushSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, newSize, resources.displayMetrics)
+        mDrawPaint!!.strokeWidth = newSize
+    }
+
+
 
     internal inner class CustomPath(var color: Int, var brushThickness: Float) : Path(){
 
--- app/src/main/java/com/avalitov/drawingapp/MainActivity.kt
+++ app/src/main/java/com/avalitov/drawingapp/MainActivity.kt
@@ -1,11 +1,57 @@
 package com.avalitov.drawingapp
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+
+lateinit var drawingView : DrawingView
+lateinit var ibBrush : ImageButton
+lateinit var smallBtn : ImageButton
+lateinit var mediumBtn : ImageButton
+lateinit var largeBtn : ImageButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        drawingView = findViewById(R.id.drawing_view)
+        ibBrush = findViewById(R.id.ib_brush)
+
+        drawingView.setBrushSize(20.toFloat())
+
+        ibBrush.setOnClickListener{
+            showBrushSizeChooserDialog()
+        }
     }
+
+    private fun showBrushSizeChooserDialog(){
+        val brushDialog = Dialog(this)
+        brushDialog.setContentView(R.layout.dialog_brush_size)
+        brushDialog.setTitle("Brush size: ")
+
+        smallBtn = brushDialog.findViewById(R.id.ib_small_brush)
+        smallBtn.setOnClickListener {
+            drawingView.setBrushSize(10.toFloat())
+            brushDialog.dismiss()
+        }
+
+        mediumBtn = brushDialog.findViewById(R.id.ib_medium_brush)
+        mediumBtn.setOnClickListener {
+            drawingView.setBrushSize(20.toFloat())
+            brushDialog.dismiss()
+        }
+
+        largeBtn = brushDialog.findViewById(R.id.ib_large_brush)
+        largeBtn.setOnClickListener {
+            drawingView.setBrushSize(30.toFloat())
+            brushDialog.dismiss()
+        }
+
+        brushDialog.show()
+
+
+    }
+
 }
